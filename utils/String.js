3d0fa89769e88358037fa161fa36d2b65e6fa60d
eexxiist <mego_dilya@mail.ru>
2023-11-30 15:42:09 +0300

    delete file

--- utils/String.js
+++
@@ -1,7 +0,0 @@
-function maxLengthString(arrayArg){
-    return arrayArg.reduce((accamulator, currentValue) => (accamulator.length <= currentValue.length) ? currentValue : accamulator , '')
-}
-
-module.exports = {
-    maxLengthString
-}
